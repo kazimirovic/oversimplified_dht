6d849305da5b3895ab94dc8d850e8a97f115130f
kazimirovic <kazimirovic@protonmail.com>
2019-03-02 20:18:29 +1000

    Run unittests as well as doctests

--- run_tests.py
+++ run_tests.py
@@ -1,6 +1,12 @@
 import doctest
+import unittest
 
 from oversimplified_dht import node, node_id
 
 doctest.testmod(node)
 doctest.testmod(node_id)
+
+loader = unittest.TestLoader()
+tests = loader.discover('.')
+testRunner = unittest.TextTestRunner()
+testRunner.run(tests)
